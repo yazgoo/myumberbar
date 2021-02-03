17eed1e404ea63fd94ebafb6f5f740bccb2da4e0
yazgoo <yazgoo@gmail.com>
2021-02-03 21:06:52 +0100

    📝 add comments

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,13 @@ use umberbar::{Conf, umberbar, WidgetPosition, Sources, Logos, ThemedWidgets, Pa
 use std::collections::HashMap;
 #[tokio::main]
 async fn main() {
+    /* width (in characters) of the bar */
     let terminal_width = 178;
+    /* color palette we will use for each bar items */
     let palette = Palette::grey_blue_cold_winter();
+    /* hash which will contain all widgets */
     let mut widgets : HashMap<WidgetPosition, Vec<Widget>> = HashMap::new();
+    /* widgets which will be aligned on the left (from left to right), built with a given theme */
     let lefts = ThemedWidgets::slash(
                 WidgetPosition::Left, vec![
                 (Sources::battery(), Logos::battery()),
@@ -15,6 +19,7 @@ async fn main() {
                 (Sources::cpu_temp(), Logos::cpu_temp()),
                 (Sources::window(terminal_width / 3), Logos::window()),
                 ], &palette);
+    /* widgets which will be aligned on the right (from right to left), built with a given theme */
     let rights = ThemedWidgets::slash(
                     WidgetPosition::Right, vec![
                     (Sources::date(), Logos::date()),
@@ -26,6 +31,7 @@ async fn main() {
         font: "DroidSansMono Nerd Font".to_string(),
         font_size: 8,
         terminal_width: terminal_width as u16,
+        /* frequency for refreshing the bar */
         refresh_time: Duration::from_secs(5),
         widgets: widgets,
     }).run().await;
