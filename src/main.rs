0dc9a9930f2651803c38e0c616e4c7b66a6b58f6
yazgoo <yazgoo@gmail.com>
2021-02-03 23:31:47 +0100

    use 0.7.3

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,13 @@ use std::collections::HashMap;
 #[tokio::main]
 async fn main() {
     /* width (in characters) of the bar */
-    let terminal_width = 178;
+    let terminal_width = 222;
     /* color palette we will use for each bar items */
-    let palette = Palette::grey_blue_cold_winter();
+    let palette = Palette::red_pink_turquoise_spring();
     /* hash which will contain all widgets */
     let mut widgets : HashMap<WidgetPosition, Vec<Widget>> = HashMap::new();
     /* widgets which will be aligned on the left (from left to right), built with a given theme */
-    let lefts = ThemedWidgets::slash(
+    let lefts = ThemedWidgets::powerline(
                 WidgetPosition::Left, vec![
                 (Sources::battery(), Logos::battery()),
                 (Sources::cpu(), Logos::cpu()),
@@ -20,7 +20,7 @@ async fn main() {
                 (Sources::window(terminal_width / 3), Logos::window()),
                 ], &palette);
     /* widgets which will be aligned on the right (from right to left), built with a given theme */
-    let rights = ThemedWidgets::slash(
+    let rights = ThemedWidgets::powerline(
                     WidgetPosition::Right, vec![
                     (Sources::date(), Logos::date()),
                     (Sources::memory(), Logos::memory()),
@@ -29,7 +29,7 @@ async fn main() {
     widgets.insert(rights.0, rights.1);
     umberbar(Conf {
         font: "DroidSansMono Nerd Font".to_string(),
-        font_size: 8,
+        font_size: 7,
         terminal_width: terminal_width as u16,
         /* frequency for refreshing the bar */
         refresh_time: Duration::from_secs(5),
